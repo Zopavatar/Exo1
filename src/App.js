4d165bb67d8af9f1a0c98672fa5e8b41e1c75d00
unknown <Charlotte Landuyt>
2023-02-06 12:47:30 +0100

    champion charolotte

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './modules/header/header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './public/css/header.css';
+
+//rafcp
+const Header = () => {
+  return (
+    <header>
+        <div id="image">
+            <div id="icone"></div>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
