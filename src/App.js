57711c5ddc192898e46d619f4cd609e30c1f8e40
unknown <Charlotte Landuyt>
2023-02-06 13:25:02 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './modules/header/header.js';
 import Section from './modules/section_a/section.js';
+import Footer from './modules/footer/footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Section/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/modules/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './public/css/footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div id="profil"></div>
+        <p>Creation of <span>Jules Wyvern</span></p>
+    </footer>
+  )
+}
+
+export default Footer;
