ef848892f70013f3ffe28b2fc906b496600e2603
unknown <Charlotte Landuyt>
2023-02-06 13:15:57 +0100

    section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './modules/header/header.js';
+import Section from './modules/section_a/section.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Section/>
     </div>
   );
 }
---
+++ src/modules/section_a/section.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./public/css/section.css";
+
+const Section = () => {
+  return (
+    <section>
+        <h1>Equilibrium #3429</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, fugit.</p>
+        <div>
+            <div>
+                <div id="icone_a"></div>
+                <p>0.041ETH</p>
+            </div>
+
+            <div>
+                <div id="icone_b"></div>
+                <p>3 days left</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Section;
